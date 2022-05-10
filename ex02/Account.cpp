3f0d271232aa881097b2e94efd97b5c850c96117
mhahn <mhahn@student.42heilbronn.de>
2022-05-10 17:15:23 +0200

    Fully implemented ex02

--- ex02/Account.cpp
+++ ex02/Account.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <iomanip>
 #include "Account.hpp"
 
 int Account::_nbAccounts = 0;
@@ -27,45 +28,87 @@ void Account::displayAccountsInfos() {
 	std::cout << "accounts:"    << _nbAccounts         << ";"
 	          << "total:"       << _totalAmount        << ";"
 			  << "deposits:"    << _totalNbDeposits    << ";"
-			  << "withdrawals:" << _totalNbWithdrawals << ";"
-			  << std::endl;
+			  << "withdrawals:" << _totalNbWithdrawals << std::endl;
 }
 
 void Account::_displayTimestamp() {
-	std::cout << "[20220504_182500] ";
+	std::time_t t = std::time(nullptr);
+    std::tm* now = std::localtime(&t);
+    std::cout << std::setfill('0')
+              << "[" << now->tm_year + 1900
+              << std::setw(2) << now->tm_mon + 1
+              << std::setw(2) << now->tm_mday
+              << "_"
+              << std::setw(2) << now->tm_hour
+              << std::setw(2) << now->tm_min
+              << std::setw(2) << now->tm_sec
+              << "] ";
 }
 
-Account::Account(int initial_deposit): _amount(initial_deposit) {
-	Account();
+Account::Account(int initial_deposit): _accountIndex(Account::_nbAccounts++), _amount(initial_deposit), _nbDeposits(0), _nbWithdrawals(0) {
+    Account::_totalAmount += initial_deposit;
+    _displayTimestamp();
+    std::cout << "index:"  << _accountIndex << ";"
+              << "amount:" << _amount       << ";"
+              << "created" << std::endl;
 }
 
-Account::Account(): _accountIndex(Account::_nbAccounts++), _nbDeposits(0), _nbWithdrawals(0) {
+Account::Account(): _accountIndex(Account::_nbAccounts++), _amount(0), _nbDeposits(0), _nbWithdrawals(0) {
 	_displayTimestamp();
-	displayStatus();
-	std::cout << "created" << std::endl;
+	std::cout << "index:"  << _accountIndex << ";"
+              << "amount:" << _amount       << ";"
+              << "created" << std::endl;
 }
 
 Account::~Account() {
-	_displayTimestamp();
-	displayStatus();
-	std::cout << "closed" << std::endl;
+    _displayTimestamp();
+    std::cout << "index:"  << _accountIndex << ";"
+              << "amount:" << _amount       << ";"
+              << "closed"  << std::endl;
 	_nbAccounts--;
-	_totalAmount -= _amount;
+	Account::_totalAmount -= _amount;
 	_totalNbDeposits -= _nbDeposits;
 	_totalNbWithdrawals -= _nbWithdrawals;
 }
 
 void Account::makeDeposit(int deposit) {
-	// TODO
+    Account::_totalNbDeposits++;
+    _nbDeposits++;
+    Account::_totalAmount += deposit;
+    _displayTimestamp();
+    std::cout << "index:"       << _accountIndex     << ";"
+              << "p_amount:"    << _amount           << ";"
+              << "deposit:"     << deposit           << ";"
+              << "amount:"      << _amount + deposit << ";"
+              << "nb_deposits:" << _nbDeposits       << std::endl;
+    _amount += deposit;
 }
 
 void Account::displayStatus() const {
-	std::cout << "index:"  << _accountIndex << ";"
-	          << "amount:" << _amount       << ";";
+    _displayTimestamp();
+	std::cout << "index:"       << _accountIndex  << ";"
+	          << "amount:"      << _amount        << ";"
+              << "deposits:"    << _nbDeposits    << ";"
+              << "withdrawals:" << _nbWithdrawals << std::endl;
 }
 
 bool Account::makeWithdrawal(int withdrawal) {
-	// TODO
+    _displayTimestamp();
+    std::cout << "index:"    << _accountIndex << ";"
+              << "p_amount:" << _amount       << ";"
+              << "withdrawal:";
+    if (withdrawal > _amount) {
+        std::cout << "refused" << std::endl;
+        return false;
+    }
+    std::cout << withdrawal << ";";
+    _amount -= withdrawal;
+    Account::_totalAmount -= withdrawal;
+    Account::_totalNbWithdrawals++;
+    _nbWithdrawals++;
+    std::cout << "amount:"         << _amount        << ";"
+              << "nb_withdrawals:" << _nbWithdrawals << std::endl;
+    return true;
 }
 
 int Account::checkAmount() const {
